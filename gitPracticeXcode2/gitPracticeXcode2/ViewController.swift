b706487e89314b19ea5fbce77aad1c269c316438
minzzzun <22000078@handong.ac.kr>
2024-05-17 16:48:34 +0900

    파일 추가

---
+++ gitPracticeXcode2/gitPracticeXcode2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitPracticeXcode2
+//
+//  Created by 김민준 on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
